f243c9dac9a89ec7f5d932fecdbb6ae2e39ccb60
Ryan <ryanshaunsmith@gmail.com>
2025-09-04 09:34:21 +0100

    set up the app

---
+++ ts-express/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import itemRoutes from "./routes/itemRoutes";
+import { errorHandler } from "./middleware/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("api/items", itemRoutes);
+
+app.use(errorHandler);
+
+export default app;
