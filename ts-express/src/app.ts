b0e8931012dd020853d8aac572a38a572611a2f0
Ryan <ryanshaunsmith@gmail.com>
2025-09-05 19:52:14 +0100

    prettier format updates

--- ts-express/src/app.ts
+++ ts-express/src/app.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import itemRoutes from "./routes/itemRoutes";
-import { errorHandler } from "./middleware/errorHandler";
+import express from 'express';
+import itemRoutes from './routes/itemRoutes';
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("api/items", itemRoutes);
+app.use('/api/items', itemRoutes);
 
 app.use(errorHandler);
 
