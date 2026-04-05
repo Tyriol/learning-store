b262eef06a51ec0326f739e457a2d018892e5783
Ryan <ryanshaunsmith@gmail.com>
2026-04-05 18:35:58 +0100

    improve readability, add reset function, and random colour function

--- intersection-observer/index.js
+++ intersection-observer/index.js
@@ -6,30 +6,45 @@ const resetBtn = document.querySelector("#resetButton");
 
 let timesSeen = 0;
 
+function getNiceRandomColor() {
+  const h = Math.floor(Math.random() * 360);
+  const s = Math.floor(Math.random() * 20) + 60; // 60–80%
+  const l = Math.floor(Math.random() * 20) + 50; // 50–70%
+
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function resetCounter() {
+  timesSeen = 0;
+  countEl.textContent = timesSeen;
+  countEl.style.color = "black";
+}
+
 h1.addEventListener("click", () => {
   alert("You clicked the heading!");
 });
 
-resetBtn.addEventListener("click", () => {
-  timesSeen = 0;
-  countEl.textContent = timesSeen;
-});
+resetBtn.addEventListener("click", resetCounter);
 
 rootDiv.addEventListener("click", () => {
   rootDiv.classList.toggle("lightblue");
 });
 
-const observer = new IntersectionObserver((entries) => {
+const handleBottomScrollIntersection = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       timesSeen++;
-      //   console.log(`The sentinel has been seen ${timesSeen} times.`);
-      console.log("The sentinel is visible in the viewport!");
-      //   observer.unobserve(target);
-      //   console.log("The observer has stopped observing the sentinel.");
+      // If you want to stop observing after the first intersection, uncomment the lines below:
+      // observer.unobserve(target);
+      // console.log("The observer has stopped observing the sentinel.");
       countEl.textContent = timesSeen;
+      countEl.style.color = getNiceRandomColor();
     }
   });
-});
+};
+
+// const handleColourChangeIntersection = (entries) => {
+
+const observer = new IntersectionObserver(handleBottomScrollIntersection);
 
 observer.observe(target);
